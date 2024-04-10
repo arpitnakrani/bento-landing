cd4665861cd768ce899c47be97d243e58fb54485
Shubham <shubhamsutariya21k@gmail.com>
2024-04-10 17:52:33 +0530

    remove unnecessary class

--- components/SocialCardsContainer.tsx
+++ components/SocialCardsContainer.tsx
@@ -9,7 +9,7 @@ interface SocialCardsContainerProps {
 const SocialCardsContainer: React.FC<SocialCardsContainerProps> = ({ socialCards }) => {
     return (
         <div className="max-w-screen-lg mx-auto  py-20">
-            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-2 gap-6 xl:grid-rows-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-2 gap-6">
                 {socialCards.map((card, index) => (
                     <div key={index} className={`col-span-1 ${card.colSpan ?? ""} ${card.rowSpan ?? ""}`}>
                         <SocialLinkCard {...card} />
